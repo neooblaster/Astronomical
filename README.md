a1e55cb3729d935e661f7bbfcfd66f60c0ff2477
Neooblaster <neo-blaster@hotmail.fr>
2017-06-01 23:07:21 +0000

    Repository Initialisation
    Publishing README.md, LICENSE and folder organisation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Astronomicals
+
+Astronomicals is a PHP Engine which contains all astronomical formulae from Astronomical Algorithm book wrote by Jean Meeus.
---
+++ tests/tests.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * tests.php
+ *
+ * %DESC BLOCK%
+ *
+ * @author Nicolas DUPRE
+ * @release 02/06/2017
+ * @version 1.0.0
+ * @package Index
+ */
+/**
+ * Version : 1.0.0 - Release : 02/06/2017 - Author : Nicolas DUPRE
+ *
+ * ### Version 1.0.0 :: xx.xx.2017
+ *
+ * • Première Release
+ *
+ *
+ * ### Description Fonctionnelle
+ *
+ *
+ * ### Détail de la fonction
+ *
+ * @param void
+ *
+ * @return void
+ */
+class AstronomicalTest extends PHPUnit_Framework_TestCase
+{
+    public function testX()
+    {
+        $this->assertEquals(0, 0);
+    }
+}
